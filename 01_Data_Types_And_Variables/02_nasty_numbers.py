3082403eca6dc7b3179046fced54dce853f78e8f
Jay <intern.pixyapp@gmail.com>
2021-07-01 21:06:52 +0530

    Renamed Folder

---
+++ 01_Data_Types_And_Variables/02_nasty_numbers.py
@@ -0,0 +1,31 @@
+# print(69420)
+
+# print(3.14159)
+
+# print(10 + 5)
+
+# print(10 - 5)
+
+# print(10 * 5)
+
+# print(10 / 5)
+
+# print(10 ++ 5)
+
+# print(10 -- 5)
+
+# print(10 ++ 5)
+
+# print(10 --- 5)
+
+# print(10 ** 5)
+
+# print(10 // 5)
+
+# print(10 % 5)
+
+# print(10 ** 2 ** 0.5)
+
+# print((10 ** 2) ** 0.5)
+
+# print(100 ** 0.5)
