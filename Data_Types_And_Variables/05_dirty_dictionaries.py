6f6b1dd7f2ceaed0f7cbe6ce9fab4d03c0993400
Jay <intern.pixyapp@gmail.com>
2021-07-01 16:20:25 +0530

    Started Pimping

---
+++ Data_Types_And_Variables/05_dirty_dictionaries.py
@@ -0,0 +1,37 @@
+# print({"1": "one", "2": "two", "3": "three"})
+
+# print({1: "one", 2: "two", 3: "three"})
+
+# print({"1": "one", 2: "two", True: "three"})
+
+
+
+# START
+
+# my_dict = {"1": "one", 2: "two", True: "three", False: "four"}
+
+# print(my_dict["1"])
+
+# print(my_dict[2])
+
+# print(my_dict[True])
+
+# print(my_dict[1])
+
+# print(my_dict[0])
+
+# print(my_dict.items())
+
+# print(my_dict.keys())
+
+# print(my_dict.values())
+
+# my_dict["1"] = "Jay"
+
+# print(my_dict)
+
+# my_dict.pop(2)
+
+# print(my_dict)
+
+# END
