7e50a61470ceb1b2d73ef5f9eb302a51dbe746d1
Jay <intern.pixyapp@gmail.com>
2021-07-02 13:04:46 +0530

    Added Cute Comments

---
+++ 00_Cute_Comments/cute_comments.py
@@ -0,0 +1,28 @@
+# This is a comment, it will not continue after this line
+as you can see # Just add # at the start to make this entire line a comment
+
+''' This is another way to add comments,
+except this can continue
+with
+as
+many
+lines
+you
+want '''
+
+"""
+This one is also a valid way to add comments
+"""
+
+# Most of the IDEs already have a shortcut to
+# comment/uncomment multiple lines at once.
+
+# The most common shortcut for this is
+# Ctrl + /
+
+# This will comment or uncomment selected lines
+
+# Most of the files in this repository have multiline comments
+# Simply uncomment them and run the code.
+
+# Good Luck!
